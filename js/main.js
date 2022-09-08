b0f6cbb4ada54cdd8418b24546829f334117e93d
Ibrahim Abdo <ibrahimporoo@gmail.com>
2022-09-08 23:50:35 +0200

    Using Fetch API instead of XMLRequest

--- js/main.js
+++ js/main.js
@@ -12,20 +12,17 @@ window.addEventListener("DOMContentLoaded", _ => {
 		day.classList.remove("active");
 	})
 	daysAmounts[today - 1].classList.add("active");
-	// ============ My Request ==============
-	let myRequest = new XMLHttpRequest();
-	myRequest.open("GET", "../data.json");
-	myRequest.send();
-	myRequest.onreadystatechange = function() {
-		if(myRequest.readyState == 4 && myRequest.status == 200) {
-			let dataJson = JSON.parse(myRequest.responseText);
-			for(let i = 0; i < dataJson.length; i++) {
-				allDays[i].textContent = dataJson[i].day;
-				activeAmounts[i].textContent = "$" + dataJson[i].amount;
-				daysAmounts[i].style.height = `${dataJson[i].amount + 40}px`;
-			}
+	// ============ Fetching Data ==============
+	fetch("../data.json").then((result) => {
+		let Data = result.json();
+		return Data;
+	}).then(Data => {
+		for(let i = 0; i < Data.length; i++) {
+			allDays[i].textContent = Data[i].day;
+			activeAmounts[i].textContent = "$" + Data[i].amount;
+			daysAmounts[i].style.height = `${Data[i].amount + 40}px`;
 		}
-	}
+	});
 })
 
 
